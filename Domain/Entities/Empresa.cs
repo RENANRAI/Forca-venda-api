86a6946d8dbb7e3d5f7901af5321b92a7f45bb15
Renan Roberto Raimundo <renanroberto@gmail.com>
2025-12-08 16:54:48 -0300

    Alteração Empresa

--- Domain/Entities/Empresa.cs
+++ Domain/Entities/Empresa.cs
@@ -1,6 +1,8 @@
 namespace ForcaVendas.Api.Domain.Entities;
 public class Empresa
 {
+
+       // alteração empresa
     public Guid Id { get; set; }
     public int CodEmp { get; set; } = default!; // codEmp
     public string Nome { get; set; } = default!;      // nome fantasia / razão social
